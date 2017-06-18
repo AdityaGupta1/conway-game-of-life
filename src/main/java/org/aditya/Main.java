1b61574c37c2438de6cb89bbd23bed96855682e5
AdityaGupta1 <adityag@gmail.com>
2017-06-17 23:36:05 -0700

    grid of clickable buttons

--- src/main/java/org/aditya/Main.java
+++ src/main/java/org/aditya/Main.java
@@ -4,20 +4,46 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main extends Application {
+    GridPane grid = new GridPane();
+
+    int width = 10;
+    int height = 10;
+
+    List<Button> buttons = new ArrayList<>();
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("game-of-life.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        stage.setTitle("Conway's Game of Life");
+
+        addButtons();
+
+        stage.setScene(new Scene(grid, 500, 500));
+        stage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void addButtons(){
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Button button = new Button("x: " + x + ", y: " + y);
+                button.setOnAction(actionEvent -> {
+                    System.out.println(button.getText());
+                });
+                grid.add(button, x, y);
+            }
+        }
     }
 }
