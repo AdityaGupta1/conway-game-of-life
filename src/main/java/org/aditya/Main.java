0c08bfa36befb5e300950c2cf094137a330279f7
AdityaGupta1 <adityag@gmail.com>
2017-06-18 11:04:39 -0700

    removed unnecessary text labels and unnecessary fxml file

--- src/main/java/org/aditya/Main.java
+++ src/main/java/org/aditya/Main.java
@@ -1,9 +1,7 @@
 package org.aditya;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -13,19 +11,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends Application {
-    GridPane grid = new GridPane();
+    private GridPane grid = new GridPane();
 
-    int width = 20;
-    int height = 20;
-    int buttonWidth = 25;
-    int buttonHeight = 25;
-    int buttonPadding = 1;
+    private int width = 20;
+    private int height = 20;
+    private int buttonWidth = 25;
+    private int buttonHeight = 25;
+    private int buttonPadding = 1;
 
     List<Button> buttons = new ArrayList<>();
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("game-of-life.fxml"));
         stage.setTitle("Conway's Game of Life");
 
         addButtons();
@@ -44,12 +41,13 @@ public class Main extends Application {
     private void addButtons() {
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                Button button = new Button("F");
+                Button button = new Button();
                 button.setOnAction(actionEvent -> {
                     toggleState(button);
                 });
                 button.setPrefSize(buttonWidth, buttonHeight);
                 GridPane.setMargin(button, new Insets(buttonPadding, buttonPadding, buttonPadding, buttonPadding));
+                button.getStyleClass().clear();
                 button.getStyleClass().add("dead");
                 grid.add(button, x, y);
             }
@@ -57,12 +55,10 @@ public class Main extends Application {
     }
 
     private void toggleState(Button button) {
-        if (button.getText().equals("F")) {
-            button.setText("T");
+        if (button.getStyleClass().get(0).equals("dead")) {
             button.getStyleClass().clear();
             button.getStyleClass().add("alive");
         } else {
-            button.setText("F");
             button.getStyleClass().clear();
             button.getStyleClass().add("dead");
         }
